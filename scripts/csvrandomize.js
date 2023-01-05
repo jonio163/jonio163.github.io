5b204a6cca1ab718c7866b923a3ffdbcdff71d76
jonio163 <jonio_not_rzk48@yahoo.co.jp>
2023-01-05 11:44:07 +0900

    Update csvrandomize.js

--- scripts/csvrandomize.js
+++ scripts/csvrandomize.js
@@ -32,9 +32,10 @@ window.onload =function(){
     }
 
     function csvArr(dataArr){
-        let arr =[];
+        let arr =[]; //2次元配列
         let j = 0;
-        let dupli = [];
+        let dupli = []; //問題IDを管理
+        let stuck = []; //csvから出力のための成形
         let list = dataArr.split('\n');
         let WordList = [];
         //帰ってきているレスポンスを配列に格納する
@@ -57,10 +58,61 @@ window.onload =function(){
                 continue;
             }
             arr[i] = list[Randomer].split(',');
+            /*
+            for(j = 0; j < 6; j++){
+                stuck[j] = arr[i][j];
+                if(i < 5){
+                    stuck[0] = "対義語"
+                }else{
+                    stuck[0] = "類義語"
+                }
+            }
+            arr[i][0] = stuck[0];
+            arr[i][1] = stuck[1];
+            arr[i][2] = ' ';
+            arr[i][3] = stuck[5];
+            */
+            
+            arr[i][0] = "(" + (i+1) +  ")";
+
+
         }
+
+        for(j = 0; j < 10; j++){
+            WordList[j] = arr[j][2];
+            delete arr[j][2];
+            delete arr[j][3];
+            delete arr[j][4];
+            delete arr[j][5];
+        }
+
+        for(j = 0; j < 5; j++){
+            arr[j][2] = arr[j+5][0];
+            arr[j][3] = arr[j+5][1];
+        }
+
+        WordList.sort((a, b) => a.localeCompare(b), 'ja'); //五十音順に並べ替え
+        arr[5] = WordList.slice();
+        arr[5][0] = "語群";
+        for(j = 1; j < 10; j++){
+            delete arr[5][j];
+        }
+        arr[6] = WordList.slice();
+
+        for(j = 0; j < 5; j++){
+            stuck[j] = WordList[j+5];
+            delete WordList[j+5];
+            delete arr[6][j+5];
+        }
+
+        arr[7] = stuck.slice();
+
+        delete arr[8];
+        delete arr[9];
+        delete arr[10];
+
         //console.log(arr);
         htmlWrite(arr);//出力をtableに設定する
-      
     }
   
     //出力のタグを設定
@@ -77,8 +129,21 @@ window.onload =function(){
         //HTMLに出力
         output.innerHTML = insert;
     }
-}
 
-function renew(){
-    location.reload();
+/*
+    function arrayShuffle(array) {
+        for(var k = (array.length - 1); 0 < k; k--){
+      
+          // 0〜(k+1)の範囲で値を取得
+          var r = Math.floor(Math.random() * (k + 1));
+      
+          // 要素の並び替えを実行
+          var tmp = array[k];
+          array[k] = array[r];
+          array[r] = tmp;
+        }
+        return array;
+    }
+*/
+   
 }
